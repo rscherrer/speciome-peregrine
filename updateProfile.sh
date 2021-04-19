cb39bc372ba731e446654ddb538ce33f27d64d36
rscherrer <raphael.scherrer@evobio.eu>
2021-04-19 22:57:42 +0200

    new file

---
+++ updateProfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "module add Python/3.6.4-foss-2018a" >> /home/$USER/.bash_profile
+echo "module add libpng/1.6.37-GCCcore-8.3.0" >> /home/$USER/.bash_profile
+echo "module add PCRE2/10.33-GCCcore-8.3.0" >> /home/$USER/.bash_profile
+echo "module add double-conversion/3.1.5-GCCcore-9.3.0" >> /home/$USER/.bash_profile
